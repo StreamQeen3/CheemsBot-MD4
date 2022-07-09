939d08c7883b4c6c2910a4be4e5e0948c9c2647b
StreamQeen3 <104708262+StreamQeen3@users.noreply.github.com>
2022-07-09 07:59:49 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.vcardowner = ['94762511600'] //ur owner number
+global.ownername = "🦄𝙷𝙴𝙻𝙻𝙾 𝙱𝚁𝙾" //ur owner name
+global.ytname = "_ලින්ක් එක නැනෙ සහෝ_" //ur yt chanel name
+global.socialm = "_ලින්ක් එක නැනෙ සහෝ_" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['94762511600']
+global.ownertag = '94762511600' //ur tag number
+global.botname = '𝙷𝙴𝙻𝙻𝙾 𝙱𝚁𝙾 Wa Bot' //ur bot name
+global.linkz = "_ලින්ක් එක නැනෙ සහෝ_" //your theme url which will be displayed on whatsapp
+global.websitex = "_ලින්ක් එක නැනෙ සහෝ_" //ur website to be displayed
+global.botscript = '_ලින්ක් එක නැනෙ සහෝ_' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "Wa Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "🦄𝙷𝙴𝙻𝙻𝙾 𝙱𝚁𝙾" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
